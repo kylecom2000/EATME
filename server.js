d4798d3dcf3af638b7121fafbaaa44f17320d897
FightingSprrow <kbauertx@gmail.com>
2018-12-06 16:50:10 -0600

    checking connection settings

--- server.js
+++ server.js
@@ -22,14 +22,15 @@ var mysql = require("mysql");
 let connection; 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
+} else {
     connection = mysql.createConnection({
         host: "localhost",
         port: 8889,
         user: "root",
         password: "root",
         database: "burgers_db"
-});
+    });
+}
 connection.connect(function(err) {
     if (err) {
         console.error("error connecting: " + err.stack);
